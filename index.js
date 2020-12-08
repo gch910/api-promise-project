e7c127eb40ce87dd3773f678156993e741528135
Gabriel Harris <gch910@gmail.com>
2020-12-08 14:58:44 -0600

    promise.all on films

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
-fetch('https://swapi.dev/api/people/1')
-    .then(res => res.json())
-        .then(person => console.log(person))
+// fetch("https://swapi.dev/api/people/1")
+//   .then((res) => res.json())
+//   .then((person) => console.log(person.name));
+
+
+
+fetch("https://swapi.dev/api/planets/1")
+  .then((res) => res.json())
+  .then((planet) => console.log(planet.name));
+
+
+
+Promise.all([
+    fetch('http://swapi.dev/api/films/1/'),
+    fetch( 'http://swapi.dev/api/films/2/'),
+    fetch('http://swapi.dev/api/films/3/'),
+    fetch('http://swapi.dev/api/films/6/')
+    
+])
+.then(results => console.log(results));
+
+
