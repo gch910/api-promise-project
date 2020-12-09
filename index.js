c85f48a08e4fd8fef6af5f1897ff6c7ad087745b
Gabriel Harris <gch910@gmail.com>
2020-12-08 18:09:18 -0600

    returning string of api data from object - functions created to assist .then chaining

--- index.js
+++ index.js
@@ -1,41 +1,56 @@
 const { writeFile, promises } = require("fs");
 const fetch = require("node-fetch");
-const fs = require("fs").promises;
 
+const getFilms = (films) => {
+  const filmPromises = films.map((film) => {
+    return fetch(film)
+      .then((res) => res.json())
+      .then((film) => film.title);
+  });
+  return Promise.all(filmPromises);
+};
 
+const getPlanet = (url) => {
+  return fetch(url)
+    .then((res) => res.json())
+    .then((planet) => planet.name);
+};
 
-// const getFilms = (films) => {
+const formatString = (data) => {
+  const string = `My name is ${data.person.name} and I am from ${
+    data.planet
+  }.\nI starred in the following films: ${data.films.join(", ")}.`;
 
-//     const filmPromises = films.map(film => {
-//         return fetch(film)
-//         .then((res) => res.json())
-//     })
-//    return Promise.all(filmPromises)
+  console.log(string);
+  return string;
+};
 
-//  }
-
-    
-// }
-
-//  fetch("https://swapi.dev/api/people/1")
-//   .then((res) => res.json())
-//   .then((person) => getFilms(person.films))
-//   .then((value) => console.log(value))
-
-
+const getData = () => {
+  let data = {};
+  return new Promise((res, rej) => {
+    fetch("https://swapi.dev/api/people/1")
+      .then((res) => res.json()) 
+      .then((person) => {
+        data.person = person;
+        return person;
+      })
+      .then((person) => getFilms(person.films))
+      .then((filmsArray) => {
+        data.films = filmsArray;
+        return data;
+      })
+      .then((data) => getPlanet(data.person.homeworld))
+      .then((planet) => {
+        data.planet = planet;
+        return res(data);
+      });
+  });
+};
+getData().then((data) => formatString(data));
 
 //fetch planet
 // fetch("https://swapi.dev/api/planets/1")
 //   .then((res) => res.json())
 //   .then((planet) => console.log(planet.name));
 
-
-
-
 //  fs.writeFile("film-info.txt", filmString())
-     
-
-
-
-
-
